9af26abbbf809fccd0c0579c7c59f54bb8d06ea4
krisprajapati03 <krisprajapati99@gmail.com>
2025-02-06 21:01:06 +0530

    first commit

---
+++ src/data.jsx
@@ -0,0 +1,39 @@
+const reviews = [
+    {
+        id: 1,
+        name: "Pranay Gupta", 
+        job: "Software Engineer", 
+        image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg", 
+        text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their dedication and expertise. Their ability to understand complex requirements and deliver high-quality solutions is truly commendable. I highly recommend them for any software development needs."
+    },
+    {
+        id: 2,
+        name: "Abir Pal",
+        job: "Graphic Designer",
+        image: "https://aeccc.targettechnology.in/static/media/AbirPal.574a09ad7cb325853b29.jpg", 
+        text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They have a keen eye for design and always deliver visually stunning graphics that meet our needs perfectly. I look forward to continuing our collaboration."
+    },
+    {
+        id: 3, 
+        name: "Soumya Banerjee", 
+        job: "Marketing Manager",
+        image: "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+        text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and innovative ideas from this team. Their strategic approach and deep understanding of the market helped us achieve our goals and exceed our expectations."
+    },
+    {
+        id: 4, 
+        name: "Saikat Mukherjee", 
+        job: "Content Strategist", 
+        image: "https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
+        text: "Working with this team has been an absolute pleasure. Their project management skills are top-notch, and they always ensure that everything runs smoothly and on time. Their professionalism and commitment to excellence are truly admirable."
+    },
+    // {
+    //     id: 5,
+    //     name: "Ananya Sharma",
+    //     job: "Project Manager",
+    //     image: "https://aeccc.targettechnology.in/static/media/AnanyaSharma.1234567890.jpg",
+    //     text: "Working with this team has been an absolute pleasure. Their project management skills are top-notch, and they always ensure that everything runs smoothly and on time. Their professionalism and commitment to excellence are truly admirable."
+    // }
+]
+
+export default reviews;
